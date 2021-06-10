43b15439ad497713acd6dd425622c28d84e2e1d7
Christian Mejia <chrismejia10@gmail.com>
2021-06-10 16:31:33 -0400

    feat(05-Callbacks): add base to keyValidator

--- 05-Callbacks-Iterators/index.js
+++ 05-Callbacks-Iterators/index.js
@@ -123,6 +123,32 @@ function coolNumbersGetSquaredSquared(nums, coolCheck, squareCools) {
 // }
 
 /**
+ * #4: keyValidator
+ * Define the function keyValidator.
+ * keyValidator accepts two objects, a testObj and a targetObj.
+ * keyValidator compares the keys of the targetObj to the ones the testObj has.
+ * keyValidator returns an array of all the keys, as strings, missing from the testObj.
+ *
+ * If the testObj has all of the same keys as the targetObj, return the string "Both objects are the same."
+ *
+ * @function keyValidator
+ * @param {Object} testObj
+ * @param {Object} targetObj
+ * @returns {String[]}
+ */
+
+function keyValidator(testObj, targetObj) {
+  let missingKeys = [];
+  const targetKeys = Object.keys(targetObj);
+  for (const testKey of Object.keys(testObj)) {
+    if (targetKeys.indexOf(testKey) === -1) {
+      missingKeys.push(testKey);
+    }
+  }
+  return missingKeys;
+}
+
+/**
  * The code below is what tests your answers.
  *
  * To test your answers to one of the problems above:
@@ -218,27 +244,39 @@ describe("05 - Callbacks and Iterators", () => {
     const allUncool = [2.2, 5.5, 8.8, 12.12];
     const mixedCool = [...allCool, allUncool];
 
-    it("returns an array", () => {
-      expect(
-        coolNumbersGetSquaredSquared(allCool, isNumberCool, coolSquaredNumbers)
-      ).to.be.an("array");
-      expect(
-        coolNumbersGetSquaredSquared(
-          allUncool,
-          isNumberCool,
-          coolSquaredNumbers
-        )
-      ).to.be.an("array");
-      expect(
-        coolNumbersGetSquaredSquared(
-          mixedCool,
-          isNumberCool,
-          coolSquaredNumbers
-        )
-      ).to.be.an("array");
+    describe("returns an array", () => {
+      it("when all the numbers are cool", () => {
+        expect(
+          coolNumbersGetSquaredSquared(
+            allCool,
+            isNumberCool,
+            coolSquaredNumbers
+          )
+        ).to.be.an("array");
+      });
+
+      it("when all the numbers are uncool", () => {
+        expect(
+          coolNumbersGetSquaredSquared(
+            allUncool,
+            isNumberCool,
+            coolSquaredNumbers
+          )
+        ).to.be.an("array");
+      });
+
+      it("when there's a mix of cool and uncool numbers", () => {
+        expect(
+          coolNumbersGetSquaredSquared(
+            mixedCool,
+            isNumberCool,
+            coolSquaredNumbers
+          )
+        ).to.be.an("array");
+      });
     });
 
-    it("removes all numbers if all numbers are ", () => {
+    it("removes all numbers if all numbers are", () => {
       console.log(allUncool.filter(isNumberCool));
       expect(
         coolNumbersGetSquaredSquared(
@@ -247,6 +285,38 @@ describe("05 - Callbacks and Iterators", () => {
           coolSquaredNumbers
         )
       ).to.eql([]);
+    });
+  });
+
+  describe("#4: keyValidator", () => {
+    const emptyOne = {};
+    const emptyTwo = {};
+    const twoKeysOne = {
+      word: "hello",
+      nums: [1, 2, 3],
+    };
+    const twoKeysTwo = {
+      word: "hello",
+      nums: [1, 2, 3],
+    };
+    const twoKeysThree = {
+      word: "banana",
+      nums: [4, 5, 6],
+    };
+    describe("returns an array", () => {
+      describe("that is empty", () => {
+        it("when both objects are empty", () => {
+          expect(keyValidator(emptyOne, emptyTwo)).to.eql([]);
+        });
+        describe("when each object has all the same keys", () => {
+          it("with the same values", () => {
+            expect(keyValidator(twoKeysOne, twoKeysTwo)).to.eql([]);
+          });
+          it("with different values", () => {
+            expect(keyValidator(twoKeysOne, twoKeysThree)).to.eql([]);
+          });
+        });
+      });
     });
   });
 });
