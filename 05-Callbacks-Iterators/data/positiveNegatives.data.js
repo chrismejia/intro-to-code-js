bb71c856ddfb21f6181a2c90139de4e7e5eba7ce
Christian Mejia <chrismejia10@gmail.com>
2021-08-10 18:06:54 -0400

    feat(05-Callbacks): remove answers; make all tests pending

---
+++ 05-Callbacks-Iterators/data/positiveNegatives.data.js
@@ -0,0 +1,6 @@
+export const a = 3;
+export const b = 5;
+export const c = -3;
+export const d = -5;
+export const e = 0;
+export const testArr = [3, 5, -3, -5, 0];
---
+++ 05-Callbacks-Iterators/data/startingCapsOnly.data.js
@@ -0,0 +1,33 @@
+export const uppers = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+export const startCapWords = ["Microsoft", "Tokyo", "Charlie"];
+export const noCapWords = ["party", "car", "banana"];
+export const capsNotAtStart = ["whOOps", "lION", "monkeY"];
+export const mixedWords = [...startCapWords, ...noCapWords, ...capsNotAtStart];
