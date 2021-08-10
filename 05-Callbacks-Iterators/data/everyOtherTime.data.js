16ce36251f66292f75f22beea0f1cbce183ea051
Christian Mejia <chrismejia10@gmail.com>
2021-08-10 17:44:15 -0400

    feat(05-Callbacks): add everyOtherTime added docs, example, data, tests

---
+++ 05-Callbacks-Iterators/data/everyOtherTime.data.js
@@ -0,0 +1,23 @@
+export const capitalize = (word) => word.toUpperCase();
+
+export const wordsArr = [
+  "abacus",
+  "banana",
+  "chocolate",
+  "diamond",
+  "elephant",
+  "fountain",
+];
+
+export const cappedArr = [
+  "abacus",
+  "BANANA",
+  "chocolate",
+  "DIAMOND",
+  "elephant",
+  "FOUNTAIN",
+];
+
+export const cubeNum = (num) => num ** 3;
+export const cubeArr = [1, 2, 3, 4, 5];
+export const evenCubed = [1, 8, 3, 64, 5];
---
+++ 05-Callbacks-Iterators/tests/07-everyOtherTime.test.js
@@ -0,0 +1,29 @@
+import { expect } from "chai";
+import sinon from "sinon";
+import {
+  capitalize,
+  wordsArr,
+  cappedArr,
+  cubeNum,
+  cubeArr,
+  evenCubed,
+} from "../data/everyOtherTime.data";
+import everyOtherTime from "../07-everyOtherTime";
+
+describe("#7: everyOtherTime", () => {
+  const capSpy = sinon.spy(capitalize);
+  const cubeSpy = sinon.spy(cubeNum);
+
+  const capResult = everyOtherTime(wordsArr, capSpy);
+  const cubeResult = everyOtherTime(cubeArr, cubeSpy);
+
+  it("runs the callback the correct number of times", () => {
+    expect(capSpy.callCount).to.equal(3);
+    expect(cubeSpy.callCount).to.equal(2);
+  });
+
+  it("returns the correct value", () => {
+    expect(capResult).to.eql(cappedArr);
+    expect(cubeResult).to.eql(evenCubed);
+  });
+});
