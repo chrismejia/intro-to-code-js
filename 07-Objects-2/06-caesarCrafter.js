b5c4723de09a4ab4036a51ce47c0ec577dd561fe
Christian Mejia <chrismejia10@gmail.com>
2023-06-26 04:08:28 -0400

    feat(07): add caesarCipher question, data base

---
+++ 07-Objects-2/06-caesarCrafter.js
@@ -0,0 +1,31 @@
+import { baseCaesar } from "./data/caesarCrafter.data";
+
+/**
+ * Define the function caesarCrafter.
+ * caesarCrafter accepts a single input, the number shiftAmount.
+ *
+ * caesarCrafter returns an object with 26 key-value pairs where the key represents the PLAINTEXT letter, and the value represents that shifted CIPHER'S letter.
+ *
+ * @see {@link https://en.wikipedia.org/wiki/Caesar_cipher Caesar cipher}
+ *
+ * @category 07 - Objects 2
+ * @function caesarCipher
+ * @returns {Object}
+ *
+ * @example
+ * const shiftedBy23 = caesarCipher(23)
+ * console.log(shiftedBy23)
+ * {
+ *   A: 'X',
+ *   B: 'Y',
+ *   C: 'Z',
+ *   ⋮   ⋮
+ *   X: 'U',
+ *   Y: 'V',
+ *   Z: 'W'
+ * }
+ */
+
+function caesarCipher(shiftAmount) {
+  return;
+}
