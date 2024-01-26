2bbcd1610d8fe9776b4dc847202176ef7b32a182
Christian Mejia <chrismejia10@gmail.com>
2024-01-25 22:02:20 -0500

    #13: [Simplify repo] 01 - Values Types (#14)
    * #13: extract tests in separate file
    * #13: rename modulo to remainder
    * #13: add typeof problem, instructions, testing
    * #13: update 01-5 instructions; add exponentiatedTo instructions; test
    * #13: simplify 01-7 test logic

--- 01-Values-and-Data-Types/index.js
+++ 01-Values-and-Data-Types/index.js
@@ -28,7 +28,7 @@ const codingIsFun = true;
 /**
  * #4: Null and undefined
  *
- * Null represents the intentional absence of value.
+ * Null represents the intentional absence of value; you can assign a variable NO value.
  * Undefined represents a lack of defined value. Variables that are declared but don't have an initial value set will be assigned the value `undefined`.
  *
  * Define two variables: isNull and isUndefined.
@@ -39,7 +39,7 @@ const isNull = null;
 const isUndefined = undefined;
 
 /**
- * #5: Quickest math class ever.
+ * #5: Quickest math class ever
  *
  * Define the variable numA. Assign it the value 15.
  * Define the variable numB. Assign it the value 3.
@@ -48,7 +48,8 @@ const isUndefined = undefined;
  * Define the variable subtractedBy. Assign it the expression (don't solve) of numA subtracted by numB.
  * Define the variable multipliedBy. Assign it the expression (don't solve) of numA being multiplied by numB.
  * Define the variable dividedBy. Assign it the expression (don't solve) of numA being divided by numB.
- * Define the variable moduloOf. Assign it the expression (don't solve) of numA being added to numB.
+ * Define the variable remainderOf. Assign it the expression (don't solve) of the remainder after dividing numA by numB.
+ * Define the variable exponentiatedTo. Assign it the expression (don't solve) of numA being raised to numB.
  *
  * Do not reassign or directly use the values of numA and numB in the math variable value defintions.
  * Do not enter the solved math expression's result for any of the math variable values.
@@ -63,10 +64,23 @@ let addedTo = numA + numB;
 let subtractedBy = numA - numB;
 let multipliedBy = numA * numB;
 let dividedBy = numA / numB;
-let moduloOf = numA % numB;
+let remainderOf = numA % numB;
+let exponentiatedTo = numA ** numB;
 
 /**
- * #6: Numbers and Words, all together
+ * #6: Values are more than values
+ *
+ * Define the variable nameType. Assign it the expression (don't solve) that determines what type myFirstName is.
+ * Define the variable ageType. Assign it the expression (don't solve) that determines what type ageType is.
+ * Define the variable funType. Assign it the expression (don't solve) that determines what type funType is.
+ */
+
+let nameType = typeof myFirstName;
+let ageType = typeof myAge;
+let funType = typeof codingIsFun;
+
+/**
+ * #7: Numbers and Words, all together
  *
  * Define the variable myDetails.
  * Assign it the value of a template literal that has the myFirstName and myAge variables embedded within. DO NOT direct embed the values into myDetails.
@@ -80,100 +94,24 @@ let moduloOf = numA % numB;
 const myDetails = `My name is ${myFirstName} and I'm ${myAge} years old.`;
 
 /**
- * The code below is what tests your answers.
- *
- * To test your answers to one of the problems above:
- * 1. Find the number of the problem you're working on
- * 2. Remove the `x` in `xdescribe` so that it reads `describe`
- * 3. Type `npm run test-01` in the Shell and hit Enter.
+ * Needed for test to work; pls do not touch
  */
-import { expect } from "chai";
-
-describe("01 - Values and Types", () => {
-  describe("#1: What's your name?", () => {
-    describe("The variable myFirstName", () => {
-      it("should be a string", () => {
-        expect(myFirstName).to.be.a("string");
-      });
-
-      it("should not be an empty string", () => {
-        expect(myFirstName).to.not.be.empty;
-      });
-    });
-  });
-
-  describe("#2: What's your age?", () => {
-    describe("The variable myAge", () => {
-      it("should be a number", () => {
-        expect(myAge).to.be.a("number");
-      });
-    });
-  });
-
-  describe("#3: In fact, it's super fun", () => {
-    describe("The variable codingIsFun", () => {
-      it("should be a boolean value", () => {
-        expect(codingIsFun).to.be.a("boolean");
-      });
-
-      it("should confirm that it's in fact fun to code ;p", () => {
-        expect(codingIsFun).to.equal(true);
-      });
-    });
-  });
-
-  describe("#4: Null and undefined", () => {
-    it("isNull -> should have a `null` value", () => {
-      expect(isNull).to.be.a("null");
-    });
-
-    it("isUndefined -> should have an `undefined` value", () => {
-      expect(isUndefined).to.be.a("undefined");
-    });
-  });
-
-  describe("#5: Quickest Math Class Ever", () => {
-    describe("Variable declarations and value assignements", () => {
-      it("numA -> assigned a number value of 15", () => {
-        expect(numA).to.be.a("number");
-        expect(numA).to.equal(15);
-      });
-      it("numB -> assigned a number value of 3", () => {
-        expect(numB).to.be.a("number");
-        expect(numB).to.equal(3);
-      });
-    });
-
-    describe("Math operations", () => {
-      it("addedTo -> expression assignment evaluates to 18", () => {
-        expect(addedTo).to.equal(18);
-      });
-      it("subtractedBy -> expression assignment evaluates to 12", () => {
-        expect(subtractedBy).to.equal(12);
-      });
-      it("multipliedBy -> expression assignment evaluates to 45", () => {
-        expect(multipliedBy).to.equal(45);
-      });
-      it("dividedBy -> expression assignment evaluates to 5", () => {
-        expect(dividedBy).to.equal(5);
-      });
-      it("moduloOf -> expression assignment evaluates to 0", () => {
-        expect(moduloOf).to.equal(0);
-      });
-    });
-  });
-
-  describe("#6: Numbers and words, all together", () => {
-    it("myDetails -> is a string", () => {
-      expect(myDetails).to.be.a("string");
-    });
-
-    it("myDetails features the value of myFirstName", () => {
-      expect(myDetails.split(" ")).to.include(`${myFirstName}`);
-    });
-
-    it("myDetails features the value of myAge", () => {
-      expect(myDetails.split(" ")).to.include(`${myAge}`);
-    });
-  });
-});
+export {
+  myFirstName,
+  myAge,
+  codingIsFun,
+  isNull,
+  isUndefined,
+  numA,
+  numB,
+  addedTo,
+  subtractedBy,
+  multipliedBy,
+  dividedBy,
+  remainderOf,
+  exponentiatedTo,
+  nameType,
+  ageType,
+  funType,
+  myDetails,
+};
