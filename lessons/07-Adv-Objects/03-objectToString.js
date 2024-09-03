ed3c131b62bdedca0b5e4a6e6e225bbbe7b9eb2f
Christian Mejia <chrismejia10@gmail.com>
2024-09-03 19:44:28 -0400

    #72: add objectToString problem to unit

---
+++ lessons/07-Adv-Objects/03-objectToString.js
@@ -0,0 +1,17 @@
+/**
+ * #3: objectToString
+ *
+ * `objectToString` function accepts an object as its input.
+ * `objectToString` returns the JSON string representation of the input object.
+ *
+ * @category 07 - Adv Objects
+ * @function objectToString
+ * @param {Object} obj - The object to be serialized into a JSON string.
+ * @returns {string} A JSON string representing the input object.
+ *
+ * @example
+ * const data = { name: 'Alice', age: 25, hobbies: ['reading', 'gaming'] };
+ * const obJAsString = objectToString(data);
+ * // obJAsString: '{"name":"Alice","age":25,"hobbies":["reading","gaming"]}'
+ */
+const objectToString = (obj) => JSON.stringify(obj);
