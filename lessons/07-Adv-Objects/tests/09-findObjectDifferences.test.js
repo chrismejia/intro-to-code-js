d82fa3bc83c2c4e650b331e58236c588a77021b0
Christian Mejia <chrismejia10@gmail.com>
2024-09-04 16:41:18 -0400

    #83: add missing test files; test data (#84)

---
+++ lessons/07-Adv-Objects/tests/09-findObjectDifferences.test.js
@@ -0,0 +1,45 @@
+import { expect } from "chai";
+import { findObjectDifferences } from "../09-findObjectDifferences";
+import {
+  objectA,
+  objectB,
+  expectedDifferencesAB,
+  objectX,
+  objectY,
+  expectedDifferencesXY,
+  objectEmpty1,
+  objectEmpty2,
+  expectedDifferencesEmpty,
+} from "../data/09-findObjectDifferences.data";
+
+xdescribe("#9: findObjectDifferences", function () {
+  it("should find differences between two objects with some differing and some matching properties", function () {
+    expect(findObjectDifferences(objectA, objectB)).to.deep.equal(
+      expectedDifferencesAB
+    );
+  });
+
+  it("should find differences when one object has additional properties", function () {
+    expect(findObjectDifferences(objectX, objectY)).to.deep.equal(
+      expectedDifferencesXY
+    );
+  });
+
+  it("should handle cases where both objects are empty", function () {
+    expect(findObjectDifferences(objectEmpty1, objectEmpty2)).to.deep.equal(
+      expectedDifferencesEmpty
+    );
+  });
+
+  it("should handle cases where one object is empty and the other has properties", function () {
+    expect(findObjectDifferences(objectEmpty1, objectY)).to.deep.equal({
+      size: [undefined, "M"],
+      color: [undefined, "blue"],
+      material: [undefined, "cotton"],
+    });
+    expect(findObjectDifferences(objectX, objectEmpty2)).to.deep.equal({
+      color: ["red", undefined],
+      size: ["M", undefined],
+    });
+  });
+});
