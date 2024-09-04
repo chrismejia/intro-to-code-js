7fc17d631da2df0dabc81c44a9bc88ba5f7339fd
Christian Mejia <chrismejia10@gmail.com>
2024-09-03 21:38:22 -0400

    #72: add manageProfiles problem

---
+++ lessons/07-Adv-Objects/tests/12-manageProfiles.test.js
@@ -0,0 +1,42 @@
+import { expect } from "chai";
+import { manageProfiles } from "../12-manageProfiles";
+import {
+  initialProfiles,
+  profileUpdates,
+  emptyProfiles,
+  emptyUpdates,
+  unchangedProfiles,
+  newProfilesOnly,
+  completeOverwriteProfiles,
+  expectedProfiles,
+  expectedNewProfilesOnly,
+  expectedCompleteOverwriteProfiles,
+} from "../data/12-manageProfiles.data";
+
+describe("#12: manageProfiles", () => {
+  it("should return the same profiles object when updates object is empty", () => {
+    const result = manageProfiles(initialProfiles, emptyUpdates);
+    expect(result).to.not.equal(initialProfiles);
+  });
+
+  it("should not change profiles that are not updated", () => {
+    const result = manageProfiles(unchangedProfiles, profileUpdates);
+    expect(result).to.deep.equal(expectedProfiles);
+  });
+
+  it("should add new profiles that are not in the initial profiles", () => {
+    const result = manageProfiles(initialProfiles, newProfilesOnly);
+    expect(result).to.deep.equal(expectedNewProfilesOnly);
+  });
+
+  it("should add new profiles from updates when profiles object is empty", () => {
+    const result = manageProfiles(emptyProfiles, profileUpdates);
+    expect(result).to.not.equal(profileUpdates);
+    expect(result).to.deep.equal(profileUpdates);
+  });
+
+  it("should correctly overwrite existing profiles with new complete data", () => {
+    const result = manageProfiles(initialProfiles, completeOverwriteProfiles);
+    expect(result).to.deep.equal(expectedCompleteOverwriteProfiles);
+  });
+});
