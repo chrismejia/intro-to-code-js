b9ec4d395bf40e02e9a5d168e5f08dd9e35639c0
Christian Mejia <Christian.Mejia@hearst.com>
2026-06-21 23:55:06 -0400

    CODE-175 convert lesson 03 tests to jest
    #173 #175

--- topics/js/lessons/03-Methods-and-Functions/tests/05-arrayToString.test.js
+++ topics/js/lessons/03-Methods-and-Functions/tests/05-arrayToString.test.js
@@ -1,19 +1,18 @@
 import { arrayToString } from "../05-arrayToString.js";
-import { expect } from "chai";
 
 describe("#5: arrayToString", () => {
   const array = ["cat", "dog", "moo"];
   describe("returns the correct string", () => {
     it("when the separator is an empty string ('')", () => {
-      expect(arrayToString(array, "")).to.equal("catdogmoo");
+      expect(arrayToString(array, "")).toBe("catdogmoo");
     });
 
     it("when the separator is a single space (' ')", () => {
-      expect(arrayToString(array, " ")).to.equal("cat dog moo");
+      expect(arrayToString(array, " ")).toBe("cat dog moo");
     });
 
     it("when the separator is any other character sequence", () => {
-      expect(arrayToString(array, "+-%")).to.equal("cat+-%dog+-%moo");
+      expect(arrayToString(array, "+-%")).toBe("cat+-%dog+-%moo");
     });
   });
 });
--- topics/js/lessons/03-Methods-and-Functions/tests/06-smallTogetherNow.test.js
+++ topics/js/lessons/03-Methods-and-Functions/tests/06-smallTogetherNow.test.js
@@ -1,18 +1,17 @@
 import { smallTogetherNow } from "../06-smallTogetherNow.js";
-import { expect } from "chai";
 
 describe("#6: smallTogetherNow", () => {
   describe("returns a unified, lowercase string", () => {
     it("when two uppercase input strings are passed in", () => {
-      expect(smallTogetherNow("HELLO", "WORLD")).to.equal("helloworld");
+      expect(smallTogetherNow("HELLO", "WORLD")).toBe("helloworld");
     });
 
     it("when two lowercase input strings are passed in", () => {
-      expect(smallTogetherNow("hello", "world")).to.equal("helloworld");
+      expect(smallTogetherNow("hello", "world")).toBe("helloworld");
     });
 
     it("when input strings of mixed cases are passed in", () => {
-      expect(smallTogetherNow("HeLLo", "WorLD")).to.equal("helloworld");
+      expect(smallTogetherNow("HeLLo", "WorLD")).toBe("helloworld");
     });
   });
 });
