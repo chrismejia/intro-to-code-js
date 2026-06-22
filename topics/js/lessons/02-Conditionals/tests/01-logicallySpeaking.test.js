aa5bec573405f139b20f8a51c10cbd9119717bb2
Christian Mejia <Christian.Mejia@hearst.com>
2026-06-21 23:52:25 -0400

    CODE-175 convert lesson 02 tests to jest
    #173 #175

--- topics/js/lessons/02-Conditionals/tests/01-logicallySpeaking.test.js
+++ topics/js/lessons/02-Conditionals/tests/01-logicallySpeaking.test.js
@@ -6,44 +6,43 @@ import { andTrue, andFalse, orTrue, orFalse } from "../index.js";
  * To test your answers to one of the problems above:
  * 1. Find the number of the problem you're working on
  * 2. Remove the `x` in `xdescribe` so that it reads `describe`
- * 3. Type `npm run test-02` in the Shell and hit Enter.
+ * 3. Type `npm run test:02` in the Shell and hit Enter.
  */
-import { expect } from "chai";
 
 describe("#1: Logically speaking", () => {
   describe("Using the && (AND) operator", () => {
     it("andTrue -> is defined", () => {
-      expect(andTrue).not.to.be.undefined;
+      expect(andTrue).not.toBeUndefined();
     });
 
     it("andTrue -> evaluates two givens with && to TRUE", () => {
-      expect(andTrue).to.be.true;
+      expect(andTrue).toBe(true);
     });
 
     it("andFalse -> is defined", () => {
-      expect(andFalse).not.to.be.undefined;
+      expect(andFalse).not.toBeUndefined();
     });
 
     it("andFalse -> evaluates two givens with && to FALSE", () => {
-      expect(andFalse).to.be.false;
+      expect(andFalse).toBe(false);
     });
   });
 
   describe("Using the || (OR) operator", () => {
     it("orTrue -> is defined", () => {
-      expect(orTrue).not.to.be.undefined;
+      expect(orTrue).not.toBeUndefined();
     });
 
     it("orTrue -> evaluates two givens with || to TRUE", () => {
-      expect(orTrue).to.be.true;
+      expect(orTrue).toBe(true);
     });
 
     it("orFalse -> is defined", () => {
-      expect(orFalse).not.to.be.undefined;
+      expect(orFalse).not.toBeUndefined();
     });
 
     it("orFalse -> evaluates two givens with || to FALSE", () => {
-      expect(orFalse).to.be.false;
+      expect(orFalse).toBe(false);
     });
   });
 });
--- topics/js/lessons/02-Conditionals/tests/02-fiveCharacters.test.js
+++ topics/js/lessons/02-Conditionals/tests/02-fiveCharacters.test.js
@@ -1,24 +1,24 @@
-import { expect } from "chai";
 import { notFiveChars, isItFiveChars } from "../index.js";
 
 describe("#2: 5 characters", () => {
   describe("notFiveChars", () => {
     it("is defined", () => {
-      expect(notFiveChars).not.to.be.undefined;
+      expect(notFiveChars).not.toBeUndefined();
     });
 
     it("is a string that isn't 5 characters long", () => {
-      expect(notFiveChars).to.be.a("string").to.not.have.lengthOf(5);
+      expect(typeof notFiveChars).toBe("string");
+      expect(notFiveChars).not.toHaveLength(5);
     });
   });
 
   describe("isItFiveChars", () => {
     it("is defined", () => {
-      expect(isItFiveChars).not.to.be.undefined;
+      expect(isItFiveChars).not.toBeUndefined();
     });
 
     it("resolves to the String `it's 5 characters`", () => {
-      expect(isItFiveChars).to.equal("not 5 characters");
+      expect(isItFiveChars).toBe("not 5 characters");
     });
   });
 });
