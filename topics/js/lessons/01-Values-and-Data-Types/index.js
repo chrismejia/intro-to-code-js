b15442e2e79f20c735734195d949d2547a3e6bcc
Christian Mejia <Christian.Mejia@hearst.com>
2026-08-12 02:59:46 -0400

    CODE-207 add lesson one MDN references

--- topics/js/lessons/01-Values-and-Data-Types/index.js
+++ topics/js/lessons/01-Values-and-Data-Types/index.js
@@ -7,10 +7,10 @@
  *    check the variable name and its `export` keyword.
  * 3. If Jest shows `Expected` and `Received`, the test loaded successfully.
  *    Compare those values and revise your answer.
- * 4. After the exercise passes, open this `01-valuesTypes.test.js` file, remove the `x`
+ * 4. After the exercise passes, open `01-valuesTypes.test.js`, remove the `x`
  *    from the next `xdescribe`, and repeat.
  *
- * Read Jest's error message throughly, it will tell you what to fix.
+ * Read Jest's error message carefully; it will help you identify what to fix.
  */
 
 /**
@@ -18,6 +18,9 @@
  *
  * Define and export the variable myFirstName.
  * Assign it the string of your name.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types#declarations
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types#string_literals
  */
 
 export const myFirstName = "name";
@@ -27,6 +30,9 @@ export const myFirstName = "name";
  *
  * Define and export the variable myAge.
  * Assign it the value of your age.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types#declarations
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types#numeric_literals
  */
 
 export const myAge = 40;
@@ -36,6 +42,8 @@ export const myAge = 40;
  *
  * Define and export the variable codingIsFun and assign it a boolean value.
  * There's only one right answer here.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types#boolean_literals
  */
 
 export const codingIsFun = true;
@@ -48,6 +56,9 @@ export const codingIsFun = true;
  *
  * Define and export two variables: isNull and isUndefined.
  * Assign the value null to isNull and the value undefined to isUndefined.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Data_structures#null_type
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Data_structures#undefined_type
  */
 
 export const isNull = null;
@@ -70,6 +81,9 @@ export const isUndefined = undefined;
  * Do not enter the solved math expression's result for any of the math variable values.
  *
  * Pass numA and numB in as variables, not as their values.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_operators#arithmetic_operators
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_precedence
  */
 
 export let numA = 15;
@@ -88,6 +102,8 @@ export let exponentiatedTo = numA ** numB;
  * Define and export the variable nameType. Assign it the expression (don't solve) that determines what type myFirstName is.
  * Define and export the variable ageType. Assign it the expression (don't solve) that determines what type ageType is.
  * Define and export the variable funType. Assign it the expression (don't solve) that determines what type funType is.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
  */
 
 export let nameType = typeof myFirstName;
@@ -103,7 +119,7 @@ export let funType = typeof codingIsFun;
  * e.g. if myFirstName = "Christian" and myAge = 32:
  * My name is Christian and I'm 32 years old.
  *
- * SEE: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#expression_interpolation
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#string_interpolation
  */
 
 export const myDetails = `My name is ${myFirstName} and I'm ${myAge} years old.`;
