ffdd869bc7a06bfa28d91a3714499bf83e1a3368
Christian Mejia <Christian.Mejia@hearst.com>
2026-08-19 19:58:10 -0400

    CODE-205 remove merge markers for unit 8 files

--- topics/js/lessons/08-Async-Await-APIs/server/server.js
+++ topics/js/lessons/08-Async-Await-APIs/server/server.js
@@ -1,16 +1,9 @@
 import express from "express";
-<<<<<<< HEAD:topics/js/lessons/08-Async-Await-APIs/server/server.js
 import { pathToFileURL } from "url";
 import collegeRouter from "./routes/college.js";
 
 const unit8Server = express();
 const PORT = process.env.PORT || 3000;
-=======
-import collegeRouter from "./routes/college.js";
-
-const unit8Server = express();
-const PORT = 3000;
->>>>>>> dev:lessons/08-Async-Await-APIs/server/server.js
 
 // Middleware
 unit8Server.use(express.json());
@@ -30,16 +23,13 @@ unit8Server.use((err, _, res) => {
   res.status(500).json({ error: "Internal Server Error" });
 });
 
-<<<<<<< HEAD:topics/js/lessons/08-Async-Await-APIs/server/server.js
 const startUnit8Server = (port = PORT) => unit8Server.listen(port);
 
-if (process.argv[1] && import.meta.url === pathToFileURL(process.argv[1]).href) {
+if (
+  process.argv[1] &&
+  import.meta.url === pathToFileURL(process.argv[1]).href
+) {
   startUnit8Server();
 }
 
 export { startUnit8Server, unit8Server };
-=======
-unit8Server.listen(PORT);
-
-export { unit8Server };
->>>>>>> dev:lessons/08-Async-Await-APIs/server/server.js
--- topics/js/lessons/08-Async-Await-APIs/server/tests/server.test.js
+++ topics/js/lessons/08-Async-Await-APIs/server/tests/server.test.js
@@ -1,8 +1,4 @@
 import request from "supertest";
-<<<<<<< HEAD:topics/js/lessons/08-Async-Await-APIs/server/tests/server.test.js
-=======
-import { expect } from "chai";
->>>>>>> dev:lessons/08-Async-Await-APIs/server/tests/server.test.js
 import { unit8Server } from "../server.js";
 
 xdescribe("08 - Async/Await & APIs Local Server", () => {
@@ -13,11 +9,7 @@ xdescribe("08 - Async/Await & APIs Local Server", () => {
         .expect(404)
         .expect("Content-Type", /json/)
         .then((response) => {
-<<<<<<< HEAD:topics/js/lessons/08-Async-Await-APIs/server/tests/server.test.js
           expect(response.body).toEqual({ error: "Route not found" });
-=======
-          expect(response.body).to.deep.equal({ error: "Route not found" });
->>>>>>> dev:lessons/08-Async-Await-APIs/server/tests/server.test.js
         });
     });
   });
